e824255fae058fc8e640d145361a0f50c1cbf8f7
Giorgio Garofalo <giogar2705@gmail.com>
2023-11-05 13:30:04 +0100

    Add top rectangles to pipes

--- scene/scene_shapes.cpp
+++ scene/scene_shapes.cpp
@@ -11,6 +11,10 @@
 #define PIPE_COLOR_TOP 0.4f, 0.67f, 0.35f
 #define PIPE_COLOR_BOTTOM 0.37f, 0.53f, 0.23f
 
+#define PIPE_TOP_WIDTH_OFFSET 2
+#define PIPE_TOP_HEIGHT 2
+
+
 Shape createBirdShape() {
     glm::vec3 color = glm::vec3(0.5, 0.3, 0.2);
     return generateRectangle(-BIRD_SIZE / 2.0f, -BIRD_SIZE / 2.0f,
@@ -32,9 +36,28 @@ Shape createGroundShape() {
 Shape createPipeShape(int type, float height) {
     glm::vec3 colorTop = glm::vec3(PIPE_COLOR_TOP);
     glm::vec3 colorBottom = glm::vec3(PIPE_COLOR_BOTTOM);
-    return generateRectangle(
+
+    // The base (big rectangle) part
+    Shape base = generateRectangle(
             -PIPE_WIDTH / 2.0f, type == SKY_PIPE ? -height : 0,
-            PIPE_WIDTH, height,
+            PIPE_WIDTH, height - PIPE_TOP_HEIGHT,
             colorBottom, colorBottom,
             colorTop, colorTop);
+
+    // The end (small rectangle) part
+    Shape top = generateRectangle(
+            -(PIPE_WIDTH + PIPE_TOP_WIDTH_OFFSET) / 2.0f, (type == SKY_PIPE ? 0 : height) - PIPE_TOP_HEIGHT,
+            PIPE_WIDTH + 2, PIPE_TOP_HEIGHT,
+            colorBottom, colorBottom,
+            colorTop, colorTop);
+
+    // Merge parts
+
+    top.verticesAmount += 6;
+    top.trianglesAmount += 2;
+
+    top.vertices.insert(top.vertices.end(), base.vertices.begin(), base.vertices.end());
+    top.colors.insert(top.colors.end(), base.colors.begin(), base.colors.end());
+
+    return top;
 }
