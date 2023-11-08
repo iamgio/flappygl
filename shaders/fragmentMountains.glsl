8d5cf7c04a7816854a1d5134accbce1be2b29779
Giorgio Garofalo <giogar2705@gmail.com>
2023-11-08 13:13:02 +0100

    Update Mountains shaders

--- shaders/fragmentMountains.glsl
+++ shaders/fragmentMountains.glsl
@@ -4,17 +4,17 @@ out vec3 color;
 
 in vec3 fragmentColor;
 
-#define POINT_FREQUENCY 300
-#define POINT_SIZE 200
+#define POINT_SPACING 500
+#define POINT_SIZE 250
 
 void main() {
-    if (int(gl_FragCoord.x * gl_FragCoord.y / 300) % POINT_FREQUENCY <= POINT_SIZE) {
+    if (int(gl_FragCoord.x * gl_FragCoord.y / POINT_SPACING) % POINT_SPACING <= POINT_SIZE) {
         // Curve patterns
-        color = vec3(0.82, 0.82, 0.7);
+        color = vec3(0.9, 0.82, 0.8);
     } /*else if (int(gl_FragCoord.x * gl_FragCoord.y * gl_FragCoord.x) % POINT_Y_FREQUENCY <= POINT_SIZE) {
         // Noise patterns
         color = vec3(0.58, 0.91, 0.96);
     }*/ else {
-        color = vec3(0.92, 0.87, 0.81);
+        color = vec3(0.92, 0.87, 0.85);
     }
 }
